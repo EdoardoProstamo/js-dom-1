83698d56ff7633032fa5b9c7a8bca2fb2d32ae8f
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-05 16:21:06 +0100

    feat: aggiunta informazioni immagine

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ console.log(L1Element);
 const btnElement = document.getElementById('btn');
 console.log(btnElement);
 
+//aggiunta eventi che si dovranno verificare se clicco sul bottone
 btnElement.addEventListener('click', function(){
+    //stampa click sul bottone
     console.log('click bottone');
+    //stampo tutto ciò che riguarda l'immagine, che comparirà quando cliccherò sul bottone
+    console.log(btnElement.src);
+    console.log(btnElement.alt);
+    console.log(btnElement.id);
+
+    
+
 })
