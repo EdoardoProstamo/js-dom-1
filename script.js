b03336fde017f83de4d6e896af713f22fa75e9f6
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-05 17:16:07 +0100

    feat: bonus

--- script.js
+++ script.js
@@ -7,24 +7,42 @@ console.log(L1Element);
 const btnElement = document.getElementById('btn');
 console.log(btnElement);
 
-//aggiunta eventi che si dovranno verificare se clicco sul bottone
-btnElement.addEventListener('click', function(){
-    //stampa click sul bottone
-    console.log('click bottone');
-    //stampo tutto ciò che riguarda l'immagine, che comparirà quando cliccherò sul bottone
-    console.log(L1Element.src);
-    console.log(L1Element.alt);
-    console.log(L1Element.id);
+// //aggiunta eventi che si dovranno verificare se clicco sul bottone
+// btnElement.addEventListener('click', function(){
+//     //stampa click sul bottone
+//     console.log('click bottone');
+//     //stampo tutto ciò che riguarda l'immagine, che comparirà quando cliccherò sul bottone
+//     console.log(L1Element.src);
+//     console.log(L1Element.alt);
+//     console.log(L1Element.id);
 
-    //imposto l'immagine che vedrò quando cliccherò sul bottone
-    L1Element.src = 'img/yellow_lamp.png';
-    L1Element.alt = 'Lampadina 2';
+//     //imposto l'immagine che vedrò quando cliccherò sul bottone
+//     L1Element.src = 'img/yellow_lamp.png';
+//     L1Element.alt = 'Lampadina 2';
 
-})
+// })
 
 
 //BONUS
-btnElement.addEventListener('click', function(){
+btnElement.addEventListener('click', function () {
     //stampa click sul bottone
-    console.log('click bottone');
+    // console.log('click bottone');
+    // //stampa classe img
+    // console.log(L1Element.className);
+
+    if (L1Element.className.contains('off')) {
+        console.log('Lampadina');
+        L1Element.className.remove('off');
+
+        L1Element.src = 'img/yellow_lamp.png';
+        L1Element.alt = 'Lampadina 2';
+        btnElement.innerText = 'Spegni';
+    }else {
+        console.log('Lampadina 2');
+        L1Element.className.add('off');
+
+        L1Element.src = 'img/white_lamp.png';
+        L1Element.alt = 'Lampadina';
+        btnElement.innerText = 'Accendi';
+    }
 })
