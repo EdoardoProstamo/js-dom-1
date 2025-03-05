9d6712977ef77ed668f307fe1c43e44fb87a9e43
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-05 16:06:40 +0100

    feat: acquisizione e stampa dei primi due elementi

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //Inseriamo in pagina l’immagine della lampadina spenta che trovate in allegato e accanto un bottone con la scritta “Accendi”.
 // Al click del bottone, la lampadina dovrà accendersi (dovremo quindi utilizzare l’immagine della lampadina accesa, sempre in allegato)
+
+const L1Element = document.getElementById('L1');
+console.log(L1Element);
+
+const btnElement = document.getElementById('btn');
+console.log(btnElement);
